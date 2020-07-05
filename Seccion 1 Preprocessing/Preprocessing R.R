89844c3a10497005517d2461a821cef2e2ba5825
felipe <juanfe_3098@live.com>
2020-07-05 17:30:42 -0500

    Sección 1 y 2, preprocesamiento de datos

--- Seccion 1 Preprocessing/Preprocessing R.R
+++ Seccion 1 Preprocessing/Preprocessing R.R
@@ -5,7 +5,6 @@
 
 # Se lee el dataset .csv ubicado en el folder Data
 DataSet <- read.csv("./Data/Data.csv")
-DataSet
 
 # Tratamiento de valores NA o nulos.
 DataSet$Age = ifelse(is.na(DataSet$Age),
@@ -15,4 +14,23 @@ DataSet$Salary = ifelse(is.na(DataSet$Salary),
                      ave(DataSet$Salary, FUN = function (x) mean(x, na.rm = TRUE)),
                      DataSet$Salary)
 
-DataSet
+# Tratamiento de variables categoricas
+DataSet$Country = factor(DataSet$Country,
+                         levels = c("France", "Spain", "Germany"),
+                         labels = c(1, 2, 3))
+DataSet$Purchased = factor(DataSet$Purchased,
+                         levels = c("Yes", "No"),
+                         labels = c(1, 0))
+
+# Dividir los datos en entrenamiento y test
+# True -> Hace parte de entrenamiento
+# False -> Hace parte de testing
+library(caTools)
+set.seed(20)
+split = sample.split(DataSet$Purchased, SplitRatio = 0.8)
+TrainingSet = subset(DataSet, split==TRUE)
+TestingSet = subset(DataSet, split==FALSE)
+
+# Escalado de variables
+TrainingSet[,2:3] = scale(TrainingSet[,2:3])
+TestingSet[,2:3] = scale(TestingSet[,2:3])
